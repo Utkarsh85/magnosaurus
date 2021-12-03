c63ed4417c6644c3bec6c9c35dd60e81e74e47f6
Utkarsh Sharma <utkarshsharma8519@gmail.com>
2021-12-03 18:20:24 +0530

    added ajv validation without defaults for beforeUpdateValidate

--- index.js
+++ index.js
@@ -12,43 +12,51 @@ let magnosaurus= function({connectionUri,ajvConfig={}}={connectionUri:"mongodb:/
 	this._connectionUri = connectionUri;
 	this._getDb= new getDb(connectionUri);
 	this._ajv = new Ajv(ajvConfig);
+	this._ajvWithouDefaults = new Ajv({
+		...ajvConfig,
+		useDefaults: false
+	});
 	ajvKeywords(this._ajv);
 	ajvFormats(this._ajv);
+	ajvKeywords(this._ajvWithouDefaults);
+	ajvFormats(this._ajvWithouDefaults);
 
-	this._ajv.addKeyword({
+	const objectIdKeyword = {
 		keyword: "isObjectId",
 		schema: false,
 		modifying: true,
 		validate: function (data, parent) {
-			if(ObjectId.isValid(data))
-			{
-				parent.parentData[parent.parentDataProperty]= ObjectId(data);
+			if (ObjectId.isValid(data)) {
+				parent.parentData[parent.parentDataProperty] = ObjectId(data);
 				return true;
-			}
-			else return false;
+			} else return false;
 		},
 		errors: true
-	})
+	};
 
-	this._ajv.addKeyword({
+	this._ajv.addKeyword(objectIdKeyword);
+	this._ajvWithouDefaults.addKeyword(objectIdKeyword);
+
+	const dateKeyword = {
 		keyword: "isDate",
 		schema: false,
 		modifying: true,
 		validate: function (data, parent) {
 
-			if((typeof data=="string" && ISODateRegex.test(data)) || (typeof data=="number") || (data instanceof Date))
-			{
-				parent.parentData[parent.parentDataProperty]= new Date(data);
+			if ((typeof data == "string" && ISODateRegex.test(data)) || (typeof data == "number") || (data instanceof Date)) {
+				parent.parentData[parent.parentDataProperty] = new Date(data);
 				return true;
-			}
-			else return false;
+			} else return false;
 		},
 		errors: true
-	})
+	};
+
+	this._ajv.addKeyword(dateKeyword);
+	this._ajvWithouDefaults.addKeyword(dateKeyword);
 }
 
 magnosaurus.prototype.model= function(modeDefinition) {
-	return new model(modeDefinition,this._getDb,this._ajv);
+	return new model(modeDefinition, this._getDb, this._ajv, this._ajvWithouDefaults);
 }
 
 magnosaurus.prototype.db= async function() {
